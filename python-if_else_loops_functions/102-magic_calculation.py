3b593b7c5caa736d2238d2b5abf24bca2c284031
Vignan-p <vignanp@koderoom.com>
2023-01-02 09:28:43 -0800

    102

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-dis.dis(magic_calculation)
+def magic_calculation(a, b, c):
+    if a < b:
+        return(c)
+    if c > b:
+        return (a + b)
+    else:
+        return ((a * b) - c)
