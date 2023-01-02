d39c36515da368c4f0fb67be91234bc8bfcccbc0
Vignan-p <vignanp@koderoom.com>
2023-01-01 22:25:59 -0800

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+2):
-    if i == q or i == e:
+for i in range(ord('a'), ord('z')+1):
+    if i == 'q' or i == 'e':
         continue
     print("{:c}".format(i), end="")
 
