c1edf6cc5265658fc659dcc25a9e1d775f808d44
Vignan-p <vignanp@koderoom.com>
2023-01-01 23:21:59 -0800

    7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 islower = __import__('7-islower').islower
 print("c is {}".format("lower" if islower("c") else "upper"))
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("! is {}".format("lower" if islower("!") else "upper"))
+print("4 is {}".format("lower" if islower("4") else "upper"))
