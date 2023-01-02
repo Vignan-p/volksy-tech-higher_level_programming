0658190dc495f1f7e8cbeec77436e843600b39d0
Vignan-p <vignanp@koderoom.com>
2023-01-02 04:14:39 -0800

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    a = number % 10
-    if number == 0:
-        return 0
-    elif number > 0:
-        return a
-    elif number < 0:
-        a = 10 - a
-        return a
+    a = abs(number) % 10
+    print(a, end="")
+
+    return a
