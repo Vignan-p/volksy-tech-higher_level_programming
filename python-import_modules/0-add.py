76a4e064605d10c380cb2ed80a9038ecf37190c2
Vignan-p <vignanp@koderoom.com>
2023-01-02 22:13:35 -0800

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-s = add(a, b)
-print(a, "+", b, "=", s)
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    s = add(a, b)
+    print(a, "+", b, "=", s)
