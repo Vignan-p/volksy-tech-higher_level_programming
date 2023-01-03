42b3a9ec09d439314cd85f606af8705618196df8
Vignan-p <vignanp@koderoom.com>
2023-01-03 03:56:27 -0800

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv)
-    if l <= 1:
+    length = len(sys.argv)
+    if length <= 1:
         print("0 arguments.")
-    elif l == 2:
-        print("{:d} argument:".format(l - 1))
+    elif length == 2:
+        print("{:d} argument:".format(length - 1))
     else:
-        print("{:d} arguments:".format(l - 1))
-    for i in range(1, l):
+        print("{:d} arguments:".format(length - 1))
+    for i in range(1, length):
         print("{:d}: {:s}".format(i, sys.argv[i]))
