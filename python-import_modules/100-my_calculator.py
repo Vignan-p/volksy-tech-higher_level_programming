21b01acd25d3b4392c6cebb7fdb1986c60eba6d1
Vignan-p <vignanp@koderoom.com>
2023-01-03 19:46:03 -0800

    100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, div, mul
+    from sys import argv
+    if len(argv) != 4:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        print(1)
+    operator = argv[2]
+    n1 = argv[1]
+    n2 = argv[2]
+    if operator == "+":
+        print("{:d} + {:d} = {:d}",format(n1, n2, add(n1, n2)))
+    elif operator == "-":
+        print("{:d} + {:d} = {:d}",format(n1, n2, sub(n1, n2)))
+    elif operator == "*":
+        print("{:d} + {:d} = {:d}",format(n1, n2, mul(n1, n2)))
+    elif operator == "/":
+        print("{:d} + {:d} = {:d}",format(n1, n2, div(n1, n2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        print(1)
