d3bb9cda0c6ad26d56bd250937041307eadf5c77
Vignan-p <vignanp@koderoom.com>
2023-01-03 19:50:59 -0800

    100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print(1)
     operator = argv[2]
     n1 = int(argv[1])
-    n2 = int(argv[2])
+    n2 = int(argv[3])
     if operator == "+":
         print("{:d} + {:d} = {:d}", format(n1, n2, add(n1, n2)))
     elif operator == "-":
