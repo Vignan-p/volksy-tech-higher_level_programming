4d90417e48e307c6b20fbbdfd6552fcd90afa518
Vignan-p <vignanp@koderoom.com>
2023-01-05 01:46:24 -0800

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     li = []
     for i in my_list:
-        if i%2 == 0:
+        if i % 2 == 0:
             li.append(True)
         else:
             li.append(False)
