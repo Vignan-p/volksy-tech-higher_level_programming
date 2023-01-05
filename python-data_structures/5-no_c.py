2b5a02d959852cb7f2458b2bd9a393ffe2763f2e
Vignan-p <vignanp@koderoom.com>
2023-01-05 03:39:42 -0800

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     copy = []
-    for x in my_string:
-        if x != "c" or x != "C":
+    for x in range(len(my_string):
+        if my_string[x] != "c" or my_string[x] != "C":
             copy.append(x)
     return "".join(copy)
