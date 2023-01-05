cfcc9b8a8aa9b20b56ee9c4d2ad9b49f24b76f15
Vignan-p <vignanp@koderoom.com>
2023-01-05 03:11:29 -0800

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    j = 0
+    j = mylist[0]
     if len(my_list) < 0:
         return None
     else:
         for i in my_list:
             if i < j:
                 continue
-            if i > j:
+            elif i > j:
                 j = i
+            else:
+                continue
         return j
