e8d5c3029f003bbadddf4d313055367619c09469
Vignan-p <vignanp@koderoom.com>
2023-01-06 02:52:29 -0800

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    list(set(set_1).symmetric_difference(set(set_2)))
+    lst = []
+    for i in set_1:
+        if i not in set_2:
+            lst.append(i)
+    for i in set_2:
+        if i not in set_1:
+            lst.append(i)
+    return lst
