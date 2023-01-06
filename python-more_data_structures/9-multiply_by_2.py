e119fad88dee795ef28eccacc03eb703d2d91772
Vignan-p <vignanp@koderoom.com>
2023-01-06 09:53:33 -0800

    9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     s = {}
     for i in a_dictionary:
-        new[i] = (a_dictionary[i] * 2)
-    return (new)
+        s[i] = (a_dictionary[i] * 2)
+    return (s)
