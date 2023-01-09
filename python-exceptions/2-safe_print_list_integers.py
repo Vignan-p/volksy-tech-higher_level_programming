138d509f789391bb772ed9676187ffa6fce004ef
Vignan-p <vignanp@koderoom.com>
2023-01-09 02:11:06 -0800

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     r = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[r]), end="")
+            print("{:d}".format(my_list[i]), end="")
             r = r + 1
         except:
             pass
