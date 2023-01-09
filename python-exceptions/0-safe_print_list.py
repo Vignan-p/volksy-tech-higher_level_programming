070a5567f9519bd22bdb72eea36aed1e3a7e54a1
Vignan-p <vignanp@koderoom.com>
2023-01-09 01:44:36 -0800

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    r = 0
     try:
-        for i in range(x):
-            print(my_list[i], end = "")
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end = "")
+            r = r + 1
+        return r
     except:
         pass
