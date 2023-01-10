3183833fae555f87d05812e7020d01da740b326d
Vignan-p <vignanp@koderoom.com>
2023-01-09 20:44:50 -0800

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
+
 def safe_print_integer_err(value):
-    import sys
     try:
         print("{:d}".format(value))
         return True
     except IndexError as e:
-        sys.stderr.write("Exception: {}".format(e))
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
