8b06181b8155eab30b9bc818997750908ac3bf9f
Vignan-p <vignanp@koderoom.com>
2023-01-11 08:52:37 -0800

    hi

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,14 +3,21 @@
 
 
 class Square:
+    """Square with private instance attribute and instatiation"""
     def __init__(self, size=0):
         """square size"""
         self.__size = size
 
+    def area(self):
+        """return area value"""
+        return (self.__size * self.__size)
+    
+    @property
     def size(self):
         """Area of square"""
         return (self.__size)
-
+    
+    @size.setter
     def size(self, value):
         """value"""
         if not isinstance(value, int):
@@ -18,7 +25,3 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """return area value"""
-        return (self.__size * self.__size)
