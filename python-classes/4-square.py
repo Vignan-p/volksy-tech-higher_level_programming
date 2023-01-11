5b13d2f956706f2280069399384abcd1148819b1
Vignan-p <vignanp@koderoom.com>
2023-01-11 04:00:48 -0800

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """square size"""
+        self.__size = size
+
+    def size(self):
+        """Area of square"""
+        return (self.__size)
+
+    def size(self, value):
+        """values"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return area value"""
+        return (self.__size * self.__size)
