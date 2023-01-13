e2631c500ce255582cb141f400ea6e66244ccc07
Vignan-p <vignanp@koderoom.com>
2023-01-13 04:10:50 -0800

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,35 +1,12 @@
 #!/usr/bin/python3
-"""
-    class Square that inherits from Rectangle (9-rectangle.py).
-    (task based on 10-square.py).
-
-    Instantiation with size: def __init__(self, size)::
-        size must be private. No getter or setter
-        size must be a positive integer, validated by
-        integer_validator
-    the area() method must be implemented
-    print() should print, and str() should return, the
-    square description: [Square] <width>/<height>
-"""
+# 1-my_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an inherited list class MyList."""
 
 
-Rectangle = __import__('9-rectangle').Rectangle
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
-
-class Square(Rectangle):
-    """ class square inherits from rectangle class """
-
-    def __init__(self, size):
-        """ initializes square """
-        super().integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
-
-    def area(self):
-        """ returns area of square """
-        return super().area()
-
-    def __str__(self):
-        """ returns [Square] <width>/<height> string """
-        a = str(self.__size)
-        return "[Square] " + a + "/" + a
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
