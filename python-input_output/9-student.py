dd84c54eb1a0f169c2dae795acdffaaae8d6d773
Vignan-p <vignanp@koderoom.com>
2023-01-18 04:14:09 -0800

    9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,15 +3,11 @@
 
 
 class Student:
-
-
     def __init__(self, first_name, last_name, age):
         """def"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
-
     def to_json(self):
         """json"""
         return vars(self)
