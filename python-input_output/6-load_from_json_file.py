6466e7ec0965ac400a83bc78c841538fa04c5ac2
Vignan-p <vignanp@koderoom.com>
2023-01-18 02:30:31 -0800

    6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """load from json"""
     with open(filename, mode='r', encoding="utf-8") as f:
-        return (json.loads(f))
+        return (json.load(f))
