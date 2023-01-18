d385bdf55da52b9deefa95831562422f78240b64
Vignan-p <vignanp@koderoom.com>
2023-01-17 22:07:53 -0800

    1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """ read a file"""
     if filename:
-        with open(filename, mode = 'r', encoding="utf-8") as f:
+        with open(filename, mode='r', encoding="utf-8") as f:
             for line in f:
                 print(line, end="")
