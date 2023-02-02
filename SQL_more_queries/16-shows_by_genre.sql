2a66a0757d2631a742ed5de2586c58858510fbe5
Vignan-p <vignanp@koderoom.com>
2023-02-02 03:30:35 -0800

    16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- 16
-SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_shows
+SELECT tv_shows.title, tv_genres.name  FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id=tv_shows.id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC;
