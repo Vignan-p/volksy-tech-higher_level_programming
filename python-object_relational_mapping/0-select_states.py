2e747ff2b6ff000dad4dc16b2d88a010437811a4
Vignan-p <vignanp@koderoom.com>
2023-02-06 21:19:20 -0800

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,22 +2,12 @@
 """SELECT command"""
 
 
-def sql_cmd(args):
-    """Connect to DATABASE"""
-    db = MySQLdb.connect(host='localhost', 
-           port=3306, 
-           user=args[0], 
-           passwd=args[1], 
-           db=args[2])
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    for i in c:
-        print(i)
-    c.close()
-    db.close()
-
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    sql_cmd(sys.argv[1:])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    for i in c:
+        print(i)
