b7f5bd2cb5f291c16cfceb5a718bd4f8b67f7598
Vignan-p <vignanp@koderoom.com>
2023-02-06 22:14:57 -0800

    2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    sql = "SELECT * FROM states WHERE name = '%s'"
+    val = (sys.argv[4], )
+    c.execute(sql, val)
+    for i in c:
+        print(i)
