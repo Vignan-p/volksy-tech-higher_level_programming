b3c6f8922c5adb351f0f09eb35730b3becf52d34
Vignan-p <vignanp@koderoom.com>
2023-02-11 20:43:36 -0800

    7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
+
 def list_state_obj():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
@@ -21,6 +22,7 @@ def list_state_obj():
         print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
 
     session.close()
+
 
 if __name__ == "__main__":
     list_state_obj()
